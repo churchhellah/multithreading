49d78ecdc5e67dcb2d6585c5a7f685e23a61ea6e
churchhellah <90222741+churchhellah@users.noreply.github.com>
2024-12-04 23:24:02 +0300

    Набрасываю скелет проекта В pom.xml добавил зависимость для lombok Объявил переменные для информации об игроке в PlayerInfo.java Переместил PlayBoard.java в отдельный пакет game и добавил константы

--- src/main/java/org/threads/dto/PlayerInfo.java
+++ src/main/java/org/threads/dto/PlayerInfo.java
@@ -1,4 +1,14 @@
 package org.threads.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.threads.game.PlayBoard;
+
+@AllArgsConstructor
+@Getter
 public class PlayerInfo {
+    private String firstName;
+    private String lastName;
+    private String ticketId;
+    private PlayBoard playBoard;
 }
