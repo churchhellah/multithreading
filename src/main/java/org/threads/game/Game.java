c7ac9dee1b0d320212ebab555847420432e5e1c3
Dmitriy Shinkarev <DShinkarev@mtsbank.ru>
2024-12-12 22:37:38 +0300

    Реализация TicketGeneratorImpl

--- src/main/java/org/threads/game/Game.java
+++ src/main/java/org/threads/game/Game.java
@@ -1,3 +1,24 @@
 package org.threads.game;
 
-public class Game {}
+import lombok.extern.slf4j.Slf4j;
+import org.threads.dto.PlayerInfo;
+import org.threads.game.impl.GameMasterImpl;
+import org.threads.game.impl.InMemoryPlayerRepositoryImpl;
+import org.threads.game.impl.LottoKegsImpl;
+import org.threads.game.impl.PlayBoardImpl;
+import org.threads.service.impl.TicketGeneratorImpl;
+
+@Slf4j
+public class Game {
+
+    private final GameMasterImpl gameMaster = new GameMasterImpl();
+    private final InMemoryPlayerRepositoryImpl playerRepository = new InMemoryPlayerRepositoryImpl();
+    private final LottoKegsImpl lottoKegs = LottoKegsImpl.getInstance();
+    private final PlayBoardImpl playBoard = new PlayBoardImpl();
+    private final TicketGeneratorImpl ticketGenerator = new TicketGeneratorImpl();
+
+    public void game() {
+        playerRepository.registerPlayer(ticketGenerator.generateTicket(), new PlayerInfo("Нина", "Лотова"));
+        playerRepository.registerPlayer(ticketGenerator.generateTicket(), new PlayerInfo("Гарик", "Харламов"));
+    }
+}
--- src/main/java/org/threads/service/impl/TicketGeneratorImpl.java
+++ src/main/java/org/threads/service/impl/TicketGeneratorImpl.java
@@ -7,12 +7,12 @@ import java.util.Random;
 public class TicketGeneratorImpl implements TicketGenerator {
     @Override
     public String generateTicket() {
-        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
-        while (salt.length() < 18) { // length of the random string.
-            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-            salt.append(SALTCHARS.charAt(index));
+        while (salt.length() < 18) { // Длина рандомной строки
+            int index = (int) (rnd.nextFloat() * CHARS.length());
+            salt.append(CHARS.charAt(index));
         }
         String ticketId = salt.toString();
         return ticketId;
